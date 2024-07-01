1c96c3792125f4c58d132ab9b236601778d1bfbe
Jenson R <mcplayer1a3@gmail.com>
2024-07-01 02:18:55 +0100

    small cleanup

--- SpectatorChat/Patch/HUD/KeyPatch.cs
+++ SpectatorChat/Patch/HUD/KeyPatch.cs
@@ -10,28 +10,30 @@ namespace SpectatorChat.Patch.HUD
     {
         internal static void Postfix(PlayerControllerB __instance)
         {
-            if ((__instance.IsOwner && (__instance.isPlayerControlled || __instance.isPlayerDead) && (!__instance.IsServer || __instance.isHostPlayerObject)) || __instance.isTestingPlayer)
+            if (!((__instance.IsOwner && (__instance.isPlayerControlled || __instance.isPlayerDead) && (!__instance.IsServer || __instance.isHostPlayerObject)) || __instance.isTestingPlayer))
             {
-                if (GlobalVariables.PlayerControllerInstance != __instance || GlobalVariables.PlayerControllerInstance == null)
+                return;
+            }
+
+            if (GlobalVariables.PlayerControllerInstance != __instance || GlobalVariables.PlayerControllerInstance == null)
+            {
+                Plugin.mls.LogInfo($"Player instance has changed due to {GlobalVariables.PlayerControllerInstance != __instance} or {GlobalVariables.PlayerControllerInstance == null}");
+                GlobalVariables.PlayerControllerInstance = __instance;
+            }
+            
+            if (__instance.isPlayerDead)
+            {
+                if (Plugin.InputActionInstance.ToggleDeathBoxKey.WasPressedThisFrame())
                 {
-                    Plugin.mls.LogInfo($"Player instance has changed due to {GlobalVariables.PlayerControllerInstance != __instance} or {GlobalVariables.PlayerControllerInstance == null}");
-                    GlobalVariables.PlayerControllerInstance = __instance;
-                }
-                
-                if (__instance.isPlayerDead)
-                {
-                    if (Plugin.InputActionInstance.ToggleDeathBoxKey.WasPressedThisFrame())
+                    if (!Plugin.KeyPressed)
                     {
-                        if (!Plugin.KeyPressed)
-                        {
-                            Generic.ToggleSpectatorBoxUI(GlobalVariables.HUDManagerInstance!, false);
-                            Plugin.KeyPressed = true;
-                        }
-                        else
-                        {
-                            Generic.ToggleSpectatorBoxUI(GlobalVariables.HUDManagerInstance!, true);
-                            Plugin.KeyPressed = false;
-                        }
+                        Generic.ToggleSpectatorBoxUI(GlobalVariables.HUDManagerInstance!, false);
+                        Plugin.KeyPressed = true;
+                    }
+                    else
+                    {
+                        Generic.ToggleSpectatorBoxUI(GlobalVariables.HUDManagerInstance!, true);
+                        Plugin.KeyPressed = false;
                     }
                 }
             }
